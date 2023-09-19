5e284ff65ad1c0c399754dac14c46b453ad3b1f5
Harshit Jain <harshitostwal1234@gmail.com>
2023-09-19 12:55:42 +0530

    Portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="flex justify-center mt-10 items-center">
-            <div className="flex justify-center items-center rounded-2xl mb-5 w-2/4 h-20 bg-cardPrimary">
+            <div className="flex justify-center items-center rounded-2xl mb-5 w-96 m-5 md:m-0 md:w-2/4 h-20 bg-cardPrimary">
                 <p className="text-primary">All Rights Reserved By Harshit Jain &copy; {new Date().getFullYear()}</p>
             </div>
         </div>
