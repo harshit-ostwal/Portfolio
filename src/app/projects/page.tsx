424fa247ccf86ad062264423857efd6ebda19a11
Harshit Jain <harshitostwal1234@gmail.com>
2023-10-16 20:32:35 +0530

    Final

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,7 @@ import {
 export default function Projects() {
     return (
         <>
-            <div className="flex flex-col mt-10 gap-10 lg:px-10">
+            <div className="flex flex-col mt-10 gap-10 lg:px-10 overflow-hidden">
                 <div>
                     <ProjectCardSection title="Projects" animation='animate-fade-in-top' data={projectCard} />
                 </div>
