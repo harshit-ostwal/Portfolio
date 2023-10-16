218fc3d27c4bce1ea60a5fee35734194c65f5310
Harshit Jain <harshitostwal1234@gmail.com>
2023-10-16 20:26:43 +0530

    Final

--- src/components/ui/ProjectsCard.tsx
+++ src/components/ui/ProjectsCard.tsx
@@ -19,14 +19,14 @@ const ProjectsCard: FC<ProjectsCardProps> = ({
 }) => {
     return (
         <Link target="_blank" href={href || '/'}>
-            <div className="flex gap-3 bg-cardPrimary rounded-lg p-5">
+            <div className="flex gap-3 bg-cardPrimary rounded-lg overflow-ellipsis overflow-hidden p-5">
                 <div
-                    className="flex py-8 px-8 rounded-md justify-center items-center"
+                    className="flex py-8 px-8 rounded-md overflow-hidden justify-center items-center"
                     style={{ backgroundColor: bgColor }}
                 >
                     <Icon />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col overflow-hidden">
                     <div className="font-bold">{title}</div>
                     <div className="text-info text-sm">{techStack}</div>
                 </div>
